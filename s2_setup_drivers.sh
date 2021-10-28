a5bb38af65f0bc5f6e3f7de5ff8f398ccd8cf45b
Vivek Sundaray <90921039+viveksray17@users.noreply.github.com>
2021-10-28 12:35:56 +0530

    Update s2_setup_drivers.sh

--- s2_setup_drivers.sh
+++ s2_setup_drivers.sh
@@ -9,23 +9,19 @@ sleep 1
 echo "Step2: Installing Grephics Drivers and microcode"
 sleep 1
 pacman -S xf86-video-ati amd-ucode --noconfirm
-echo -e "\nDone\n\n"
+echo -e "\nDONE\n"
 sleep 1
 
 echo "Removing Screen Tearing"
 sleep 1
 touch /etc/X11/xorg.conf.d/20-radeon.conf
 echo 'Section "Device"
-	Identifier  "Radeon"
-	Driver "radeon"
-	Option "AccelMethod" "glamor"
+    Identifier  "Radeon"
+    Driver "radeon"
+    Option "AccelMethod" "glamor"
     Option "DRI" "3"
     Option "TearFree" "on"
     Option "ColorTiling" "on"
     Option "ColorTiling2D" "on"
 EndSection' > /etc/X11/xorg.conf.d/20-radeon.conf
-echo -e "Done\n\n"
-
-sleep 1
-
-echo "\n\n***PLEASE REBOOT THE SYSTEM***\n\n"
+echo "DONE"
