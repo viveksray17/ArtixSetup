53859baea9294c388d20002a11a9de4722704156
Vivek Sundaray <viveksundaray123@outlook.com>
2021-10-30 21:15:23 +0530

    Removed some important but time taking aur packages which i can install later

--- s3_aur_packages.sh
+++ s3_aur_packages.sh
@@ -1,3 +1,3 @@
 sudo cp /data/vivek/extra/Backups/files/makepkg.conf /etc/makepkg.conf
 # aur packages
-yay -S sxiv-git libxft-bgra-git ttf-ms-fonts heroku-cli-bin vscodium-bin zsh-completions-git zsh-syntax-highlighting-git nitrogen-git --needed
+yay -S sxiv-git libxft-bgra-git ttf-ms-fonts zsh-completions-git zsh-syntax-highlighting-git nitrogen-git --needed
