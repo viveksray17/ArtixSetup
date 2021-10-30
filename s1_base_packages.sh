68b29b9771a48799771ea4a1ea426922465da1d9
Vivek Sundaray <viveksundaray123@outlook.com>
2021-10-30 22:38:30 +0530

    Added a Necessary Packages
    I added gvfs-mtp so that android-file-transfer works well.

--- s1_base_packages.sh
+++ s1_base_packages.sh
@@ -2,4 +2,4 @@ echo -e "ADDING UNIVERSE REPOSITORY"
 sudo cp /data/vivek/extra/Backups/files/pacman.conf /etc/pacman.conf
 # update repositories
 sudo pacman -Syy
-sudo pacman -S fuse2 base-devel wget gvfs pcmanfm man noto-fonts noto-fonts-emoji pavucontrol pulseaudio-alsa pulseaudio picom openssh lxappearance zsh arc-gtk-theme ttf-liberation qt5ct mpv brightnessctl xorg xorg-xinit librewolf scrot gtk-engine-murrine unzip --needed
+sudo pacman -S fuse2 base-devel wget gvfs gvfs-mtp pcmanfm man noto-fonts noto-fonts-emoji pavucontrol pulseaudio-alsa pulseaudio picom openssh lxappearance zsh arc-gtk-theme ttf-liberation qt5ct mpv brightnessctl xorg xorg-xinit librewolf scrot gtk-engine-murrine unzip --needed
