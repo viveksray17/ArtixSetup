2000e790ec3ffcbd248059ce8b2256b8cc6cbdc6
Vivek Sundaray <viveksundaray123@outlook.com>
2021-11-02 10:12:12 +0530

    Changed Librewolf to firefox due to some missing features

--- s3_aur_packages.sh
+++ s3_aur_packages.sh
@@ -1,3 +1,3 @@
 sudo cp /data/vivek/extra/Backups/files/makepkg.conf /etc/makepkg.conf
 # aur packages
-yay -S sxiv-git libxft-bgra-git ttf-ms-fonts zsh-completions-git zsh-syntax-highlighting-git nitrogen-git --needed
+yay -S firefox-beta-bin sxiv-git libxft-bgra-git ttf-ms-fonts zsh-completions-git zsh-syntax-highlighting-git nitrogen-git --needed
