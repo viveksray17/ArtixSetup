498f53a80e6d9d28d6e6e4ebea5442e859924374
Vivek Sundaray <viveksundaray123@outlook.com>
2021-12-19 13:16:57 +0530

    Added some more packages

--- s1_base_packages.sh
+++ s1_base_packages.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo pacman -S python curl base-devel wget gvfs gvfs-mtp pcmanfm man noto-fonts noto-fonts-emoji pavucontrol pulseaudio pulseaudio-alsa picom openssh lxappearance gnome-themes-extra ttf-liberation mpv brightnessctl xorg-server xorg-xinit xorg-xmodmap scrot gtk-engine-murrine unzip htop neofetch bash-completion dunst libnotify pass xclip dash python-pip feh --needed
+sudo pacman -S python curl base-devel wget gvfs gvfs-mtp pcmanfm man noto-fonts noto-fonts-emoji pavucontrol pulseaudio pulseaudio-alsa picom openssh lxappearance gnome-themes-extra ttf-liberation mpv brightnessctl xorg-server xorg-xinit xorg-xmodmap scrot gtk-engine-murrine unzip htop neofetch bash-completion dunst libnotify pass xclip dash python-pip feh dbus-glib mailcap xarchiver --needed
