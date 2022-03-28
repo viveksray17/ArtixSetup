0c9895025b5801b98e945dac40822dd63a595a93
Vivek Sundaray <viveksundaray123@outlook.com>
2022-03-28 20:41:28 +0530

    did some changes

--- s1_base_packages.sh
+++ s1_base_packages.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sudo pacman -S python curl base-devel wget gvfs gvfs-mtp pcmanfm man noto-fonts noto-fonts-emoji pulseaudio pulseaudio-alsa picom openssh lxappearance gnome-themes-extra ttf-liberation mpv brightnessctl xorg-server xorg-xinit xorg-xmodmap scrot gtk-engine-murrine unzip htop neofetch bash-completion dunst libnotify pass xclip dash python-pip feh dbus-glib mailcap xorg-util-macros --needed
-pip install pulsemixer
+sudo pacman -S curl wget sxiv rclone fuse2 ripgrep gvfs gvfs-mtp docker docker-runit pcmanfm man noto-fonts noto-fonts-emoji pulseaudio pulseaudio-alsa picom openssh lxappearance gnome-themes-extra ttf-liberation mpv brightnessctl xorg-server xorg-xinit xorg-xmodmap maim gtk-engine-murrine unzip htop neofetch bash-completion pass xclip python-pip feh xorg-util-macros --needed
--- s4_setup_personal_backups.sh
+++ s4_setup_personal_backups.sh
@@ -1,46 +1,8 @@
-#!/bin/bash
-sudo mkdir /secrets
-sudo mount /dev/sda4 /secrets
-
-directories=(
-    ssh
-    gnupg
-)
-for DIR in "${directories[@]}"; do
-    dir=~/.$DIR
-    if [ -d "$dir" ]; then
-        rm -rf "$dir"
-    fi
-    cp /secrets/vivek/$DIR $dir -r
-done
 # NVIM SETUP
 echo -e "\nINSTALLING vim-plug for neovim"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 echo -e "DONE"
-
-echo -e "\nSetting Up GRUB"
-sudo cp /secrets/vivek/files/grub /etc/default/grub
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-
-sudo cp /secrets/vivek/fonts/* /usr/share/fonts/ -R
-sudo fc-cache -fv
-
-echo -e "\nCOPYING ICONS SO MAY TAKE SOME TIME"
-sudo cp /secrets/vivek/Icons/* /usr/share/icons -R
-echo -e "DONE\n"
-sudo umount -R /secrets
-
-echo -e "\nSETTING UP PASSWORDS"
-DIR=~/.password-store
-if [ -d "$DIR" ]; then
-    rm -rf "$DIR"
-fi
-git clone git@github.com:viveksray17/passwords ~/.password-store
-cd ~/.password-store
-
-# Set Default binsh to dash
-sudo ln -sfn dash /bin/sh
 
 echo -e "\nGetting Wallpapers"
 mkdir -p $HOME/Pictures/Screenshots $HOME/Pictures/wallpapers
